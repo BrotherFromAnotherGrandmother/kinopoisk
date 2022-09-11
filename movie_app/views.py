f254e090b2495d40341bda8d885b4dea0e53be2e
BrotherFromAnotherGrandmother <101392966+BrotherFromAnotherGrandmother@users.noreply.github.com>
2022-09-11 18:02:22 +0300

    Редактировал по PEP8 Добавил вычисляемые поля с annotate

--- movie_app/views.py
+++ movie_app/views.py
@@ -1,10 +1,20 @@
 from django.shortcuts import render, get_object_or_404
 from movie_app.models import Movie
-from django.db.models import F, Sum, Min, Max, Count, Avg
+from django.db.models import F, Sum, Min, Max, Count, Avg, Value
+
+
 # Create your views here.
 
 def show_all_movie(request):
-    movies = Movie.objects.order_by(F('year').asc(nulls_last=True), 'rating')
+    # movies = Movie.objects.order_by(F('year').asc(nulls_last=True), 'rating')
+    movies = Movie.objects.annotate(
+        true_bool=Value(True),
+        false_bool=Value(False),
+        str_field=Value('hello'),
+        int_field=Value(123),
+        new_budget=F('budget') + 100,
+        new_cadabra=F('rating') + F('year'),
+    )
     agg = movies.aggregate(Avg('budget'), Max('rating'), Min('rating'), Count('id'))
     return render(request, 'movie_app/all_movies.html', {
         'movies': movies,
@@ -12,7 +22,8 @@ def show_all_movie(request):
         'total': movies.count(),
     })
 
-def show_one_movie(request, slug_movie:str):
+
+def show_one_movie(request, slug_movie: str):
     movie = get_object_or_404(Movie, slug=slug_movie)
     return render(request, 'movie_app/one_movie.html', {
         'movie': movie
