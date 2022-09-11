b4f15a5836295551edddeab35ae584f243deaafd
BrotherFromAnotherGrandmother <101392966+BrotherFromAnotherGrandmother@users.noreply.github.com>
2022-09-11 15:51:25 +0300

    Добавил функции агрегации в словарь show_all_movies

--- movie_app/views.py
+++ movie_app/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render, get_object_or_404
 from movie_app.models import Movie
-from django.db.models import F
+from django.db.models import F, Sum, Min, Max, Count, Avg
 # Create your views here.
 
 def show_all_movie(request):
     movies = Movie.objects.order_by(F('year').asc(nulls_last=True), 'rating')
+    agg = movies.aggregate(Avg('budget'), Max('rating'), Min('rating'))
     return render(request, 'movie_app/all_movies.html', {
-        'movies': movies
+        'movies': movies,
+        'agg': agg,
     })
 
 def show_one_movie(request, slug_movie:str):
