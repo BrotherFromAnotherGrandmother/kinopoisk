a4f52e9c52efd8529d53d651bf3363bce0893ac4
BrotherFromAnotherGrandmother <101392966+BrotherFromAnotherGrandmother@users.noreply.github.com>
2022-09-15 16:16:18 +0300

    Добавил поле currency. Создал и накатил миграцию

--- movie_app/models.py
+++ movie_app/models.py
@@ -2,12 +2,21 @@ from django.db import models
 from django.urls import reverse
 from django.utils.text import slugify
 
+
 # Create your models here.
 class Movie(models.Model):
+
+    CURRENCY_CHOICES = [
+        ('E', 'Euro'),
+        ('D', 'Dollar'),
+        ('R', 'Rubles'),
+    ]
+
     name = models.CharField(max_length=40)
     rating = models.IntegerField()
     year = models.IntegerField(null=True, blank=True)
     budget = models.IntegerField(default=1000000)
+    currency = models.CharField(max_length=1, choices=CURRENCY_CHOICES, default='R')
     slug = models.SlugField(default='', null=False, db_index=True)
 
     def get_url(self):
@@ -16,7 +25,6 @@ class Movie(models.Model):
     def save(self, *args, **kwargs):
         self.slug = slugify(self.name)
         super(Movie, self).save(*args, **kwargs)
-
 
     def __str__(self):
         return f'{self.name} - {self.rating}%'
