a7293254b353a84cf0bea10790c81dae4d4bf502
BrotherFromAnotherGrandmother <101392966+BrotherFromAnotherGrandmother@users.noreply.github.com>
2022-09-18 18:21:13 +0300

    Удалил все изменения коммитов, начиная с коммита  "Изменил свойство PROTECT на свойство CASCADE BrotherFromAnotherGrandmother Yesterday 12:17"(изменения в нём не удалял)

--- movie_app/urls.py
+++ movie_app/urls.py
@@ -5,6 +5,4 @@ from . import views
 urlpatterns = [
     path('', views.show_all_movie),
     path('movie/<slug:slug_movie>', views.show_one_movie, name='movie-detail'),
-    path('directors/<slug:slug_director>', views.show_one_director, name='director-detail'),
-    path('directors/', views.show_all_director),
 ]
