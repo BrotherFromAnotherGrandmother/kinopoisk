9f9e07d3b72ead7288d2cd92836bdad1ff1d15b5
BrotherFromAnotherGrandmother <101392966+BrotherFromAnotherGrandmother@users.noreply.github.com>
2022-09-19 13:29:58 +0300

    Создал функцию-представление для списка актёров. Импортировал Actor из models.py в views.py Удалил ненужный комментарий

--- movie_app/views.py
+++ movie_app/views.py
@@ -1,12 +1,11 @@
 from django.shortcuts import render, get_object_or_404
-from movie_app.models import Movie
+from movie_app.models import Movie, Actor
 from django.db.models import F, Sum, Min, Max, Count, Avg, Value
 
 
 # Create your views here.
 
 def show_all_movie(request):
-    # movies = Movie.objects.order_by(F('year').asc(nulls_last=True), 'rating')
     movies = Movie.objects.annotate(
         true_bool=Value(True),
         false_bool=Value(False),
@@ -27,4 +26,10 @@ def show_one_movie(request, slug_movie: str):
     movie = get_object_or_404(Movie, slug=slug_movie)
     return render(request, 'movie_app/one_movie.html', {
         'movie': movie
+    })
+
+def show_all_actors(request):
+    actors = Actor.objects.all()
+    return render(request, 'movie_app/all_movies.html', {
+        'actors': actors,
     })
