dd5daac333cf3ae517c3eb991c7251eda706f296
BrotherFromAnotherGrandmother <101392966+BrotherFromAnotherGrandmother@users.noreply.github.com>
2022-09-20 20:16:49 +0300

    Связал функцию-представление списка актёров с шаблоном Создал шаблон отображения списка актёров

--- movie_app/views.py
+++ movie_app/views.py
@@ -30,6 +30,6 @@ def show_one_movie(request, slug_movie: str):
 
 def show_all_actors(request):
     actors = Actor.objects.all()
-    return render(request, 'movie_app/all_movies.html', {
+    return render(request, 'movie_app/all_actors.html', {
         'actors': actors,
     })
