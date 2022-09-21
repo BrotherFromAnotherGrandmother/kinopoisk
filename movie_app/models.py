3f10a848bfffffcbacfe2ab689dec9bb637ac5f3
BrotherFromAnotherGrandmother <101392966+BrotherFromAnotherGrandmother@users.noreply.github.com>
2022-09-21 16:20:36 +0300

    Удалил шаблон all_actors.html для списка актёров Удалил метод get_url в модели Actor Дополнил шаблон one_movie.html

--- movie_app/models.py
+++ movie_app/models.py
@@ -32,8 +32,6 @@ class Actor(models.Model):
         else:
             return f'Актрисса {self.first_name} {self.last_name}'
 
-    def get_url(self):
-        return reverse('movie-detail', args=[self.slug])
 
 class Movie(models.Model):
     EUR = 'EUR'
