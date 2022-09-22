37de53cc26be4c0598a3b83ebada75afdd992281
BrotherFromAnotherGrandmother <101392966+BrotherFromAnotherGrandmother@users.noreply.github.com>
2022-09-22 15:39:00 +0300

    Установил атрибут related_name='movies' (позволяет дать псевдоним операции movie_set. при поиске всех фильмов снятых режиссёром (связь ManyToMany))

--- movie_app/models.py
+++ movie_app/models.py
@@ -57,7 +57,7 @@ class Movie(models.Model):
                                  validators=[MinValueValidator(1)])
     currency = models.CharField(max_length=3, choices=CURRENCY_CHOICES, default=EUR)
     slug = models.SlugField(default='', null=False, db_index=True)
-    director = models.ForeignKey(Director, on_delete=models.CASCADE, null=True)
+    director = models.ForeignKey(Director, on_delete=models.CASCADE, null=True, related_name='movies')
     actors = models.ManyToManyField(Actor)
 
     def get_url(self):
