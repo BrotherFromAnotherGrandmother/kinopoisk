0dd4f05391d7ce510aa563f20877e41bbc877f9d
BrotherFromAnotherGrandmother <101392966+BrotherFromAnotherGrandmother@users.noreply.github.com>
2022-09-23 13:46:14 +0300

    Создал новую модель DressingRoom. Соединил её с моделью Actor через поле OneToOneField

--- movie_app/models.py
+++ movie_app/models.py
@@ -17,6 +17,14 @@ class Director(models.Model):
         return reverse('director-detail', args=[self.id])
 
 
+class DressingRoom(models.Model):
+    floor = models.IntegerField
+    number = models.IntegerField
+
+    def __str__(self):
+        return f'{self.floor} {self.number}'
+
+
 class Actor(models.Model):
     MALE = 'M'
     FEMALE = 'F'
@@ -28,6 +36,8 @@ class Actor(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     gender = models.CharField(max_length=1, choices=GENDERS, default=MALE)
+    dressing = models.OneToOneField(DressingRoom, on_delete=models.SET_NULL, null=True,
+                                    blank=True)
 
     def __str__(self):
         if self.gender == self.MALE:
